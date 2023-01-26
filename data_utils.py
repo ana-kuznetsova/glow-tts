3ed5664beb7bbf4d12dfd99cc0ea4a1a303924ea
Anastasia  Kuznetsova <menina.indigena.17@gmail.com>
2023-01-25 20:06:35 -0500

    add mel pair data loader

--- data_utils.py
+++ data_utils.py
@@ -9,6 +9,64 @@ from text import text_to_sequence, cmudict
 from text.symbols import symbols
 
 
+class Mel2MelLoader(torch.utils.data.Dataset):
+    """
+        1) loads source <-> target audio paird
+        3) computes mel-spectrograms from audio files.
+    """
+    def __init__(self, audiopaths_and_text, hparams):
+        self.audiopaths_and_text = load_filepaths_and_text(audiopaths_and_text)
+        #List of format [[source_path, target_path]]
+        #self.text_cleaners = hparams.text_cleaners
+        self.max_wav_value = hparams.max_wav_value
+        self.sampling_rate = hparams.sampling_rate
+        self.load_mel_from_disk = hparams.load_mel_from_disk
+        self.add_noise = hparams.add_noise
+        #self.add_blank = getattr(hparams, "add_blank", False) # improved version
+        #if getattr(hparams, "cmudict_path", None) is not None:
+        #  self.cmudict = cmudict.CMUDict(hparams.cmudict_path)
+        self.stft = commons.TacotronSTFT(
+            hparams.filter_length, hparams.hop_length, hparams.win_length,
+            hparams.n_mel_channels, hparams.sampling_rate, hparams.mel_fmin,
+            hparams.mel_fmax)
+        random.seed(1234)
+        random.shuffle(self.audiopaths_and_text)
+
+    def get_mel_mel_pair(self, audiopath_and_text):
+        # separate filename and text
+        source_audiopath, target_audiopath = audiopath_and_text[0], audiopath_and_text[1]
+        source_mel = self.get_mel(source_audiopath)
+        target_mel = self.get_mel(target_audiopath)
+        return (source_mel, target_mel)
+
+    def get_mel(self, filename):
+        if not self.load_mel_from_disk:
+            audio, sampling_rate = load_wav_to_torch(filename)
+            if sampling_rate != self.stft.sampling_rate:
+                raise ValueError("{} {} SR doesn't match target {} SR".format(
+                    sampling_rate, self.stft.sampling_rate))
+            if self.add_noise:
+                audio = audio + torch.rand_like(audio)
+            audio_norm = audio / self.max_wav_value
+            audio_norm = audio_norm.unsqueeze(0)
+            melspec = self.stft.mel_spectrogram(audio_norm)
+            melspec = torch.squeeze(melspec, 0)
+        else:
+            melspec = torch.from_numpy(np.load(filename))
+            assert melspec.size(0) == self.stft.n_mel_channels, (
+                'Mel dimension mismatch: given {}, expected {}'.format(
+                    melspec.size(0), self.stft.n_mel_channels))
+
+        return melspec
+
+    def __getitem__(self, index):
+        return self.get_mel_mel_pair(self.audiopaths_and_text[index])
+
+    def __len__(self):
+        return len(self.audiopaths_and_text)
+
+
+
 class TextMelLoader(torch.utils.data.Dataset):
     """
         1) loads audio,text pairs
